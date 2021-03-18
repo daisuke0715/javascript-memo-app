e2444e176d4804e6035cba6b0594f961eb0eea3c
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2021-03-18 21:50:30 +0900

    commit-modified-first-task

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const taskTb = document.getElementById('task-table');
-const taskTbBody = document.getElementById('task-table-body');
+let taskTbBody = document.getElementById('task-table-body');
 const createButton = document.getElementById('create-button');
 
 // 入力された情報が格納されている配列
 const todos = [];
+
 
 // 追加ボタンクリック時の挙動
 createButton.addEventListener('click', () => {
@@ -14,11 +15,16 @@ createButton.addEventListener('click', () => {
         task: '入力された情報',
         status: '作業中'
     };
-    
+
+    while (taskTbBody.children[1] !== undefined) {
+        taskTbBody.removeChild(taskTbBody.children[1]);
+    }
+
     todo.task = document.getElementById('task-input-form').value;
     todos.push(todo);
 
     displayTodos(todos);
+    
 });
 
 
@@ -38,15 +44,17 @@ const escapeHtml = (str) => {
 // todos配列の中身を表示させる関数
 const displayTodos = (todosArray) => {
     console.log(todosArray);
-    for (let i = 0; i <= todosArray.length; i++) {
+    for (let i = 0; i < todosArray.length; i++) {
         // 列の生成
         const tbRow = document.createElement('tr');
+        tbRow.classList.add('tr');
 
         // セルの生成
         const idCell = document.createElement('td');
         const taskCell = document.createElement('td');
         const statusCell = document.createElement('td');
         const deleteCell = document.createElement('td');
+        
 
         // セル内の要素の生成
         const idText = document.createTextNode(i);
@@ -60,21 +68,23 @@ const displayTodos = (todosArray) => {
         deleteButton.type = 'button';
         deleteButton.value = '削除';
 
-        // 生成したセルの子要素に要素を追加
-        idCell.appendChild(idText);
-        taskCell.appendChild(taskText);
-        statusCell.appendChild(statusButton);
-        deleteCell.appendChild(deleteButton);
-
         // セルを生成した列に挿入
-        tbRow.appendChild(idCell);
-        tbRow.appendChild(taskCell);
-        tbRow.appendChild(statusCell);
-        tbRow.appendChild(deleteCell);
+        tbRow.appendChild(idCell).appendChild(idText);
+        tbRow.appendChild(taskCell).appendChild(taskText);
+        tbRow.appendChild(statusCell).appendChild(statusButton);
+        tbRow.appendChild(deleteCell).appendChild(deleteButton);
 
         // 完成した列を画面に追加
+        // taskTbBody.innerHTML(tbRow);
+
         taskTbBody.appendChild(tbRow);
     }
 }
 
 
+
+
+
+// オブジェクト指向プログラミングのメリット
+// 今回のエラーに気づいた方法
+// 上記2点について自分なりの仮説を共有した状態で、質問する
