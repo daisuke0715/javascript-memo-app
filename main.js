d8c2433d2cc384890a4796765d37eac62ab0ed98
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2021-03-29 18:24:04 +0900

    commit-for-review-3-4

--- main.js
+++ main.js
@@ -3,8 +3,17 @@
 const taskTb = document.getElementById('task-table');
 const taskTbBody = document.getElementById('task-table-body');
 const createButton = document.getElementById('create-button');
+const radioStatuses = document.getElementsByName('radio-status');
 
 const todos = [];
+
+window.onload = () => {
+    radioStatuses.forEach((e) => {
+        e.addEventListener('click', () => {
+            checkStatus();
+        });
+    });
+}
 
 createButton.addEventListener('click', () => {
 
@@ -16,11 +25,11 @@ createButton.addEventListener('click', () => {
     todo.task = document.getElementById('task-input-form').value;
     todos.push(todo);
 
-    displayTodos(todos);
+    checkStatus();
 
     document.getElementById('task-input-form').value = '';
-    
 });
+
 
 
 const displayTodos = (todosArray) => {
@@ -67,17 +76,52 @@ const displayTodos = (todosArray) => {
     }
 }
 
-
 const statusChange = index => {
     if (todos[index].status === '作業中' ) {
         todos[index].status = '完了';
     } else {
         todos[index].status = '作業中';
-    } 
-    displayTodos(todos);
+    }
+    checkStatus();
 }
+
 
 const deleteTask = index => {
     todos.splice(index, 1);
-    displayTodos(todos);
+    checkStatus();
+}
+
+
+const checkStatus = () => {
+    let nowStatus = '';
+
+    // 現在のステータス情報を取得
+    for (let i = 0; i < radioStatuses.length; i++){
+        if (radioStatuses[i].checked) {
+            nowStatus = radioStatuses[i].id;
+            break;
+        }
+    }
+
+    let newTodos = [];
+    // ステータスによって表示させるタスク内容を変更する
+    if (nowStatus === 'doing') {
+        todos.filter((value) => {
+            if (value.status === '作業中') {
+                newTodos.push(value);
+                displayTodos(newTodos);
+            }
+        });
+    }else if (nowStatus === 'done') {
+        todos.filter((value) => {
+            if (value.status === '完了') {
+               newTodos.push(value);
+               displayTodos(newTodos);
+            }
+        });
+    }else {
+        displayTodos(todos);
+    }
+
+    
 }
